954839bd50a3a24cc52b708e4dffafcf1b13ba7f
CedThen <50086303+CedThen@users.noreply.github.com>
2025-05-12 20:33:36 -0400

    adds basic route and location classes

---
+++ scripts/map/route.gd
@@ -0,0 +1,34 @@
+@tool
+class_name Route extends Line2D
+
+@onready var color_rect: ColorRect = $ColorRect
+@onready var weight_label: Label = $ColorRect/WeightLabel
+
+@export var destination: Location
+@export var line_color: Color = Color(1, 0, 0)
+@export var weight: float = 1.0
+
+# need to query some source to check if prereqs are met
+var prereqs
+
+func _ready() -> void:
+	render_route()
+
+func _process(delta: float) -> void:
+	if Engine.is_editor_hint():
+		render_route()
+
+func render_route():
+	if destination:
+		points[0] = Vector2.ZERO
+		var end_point = destination.global_position - global_position
+		points[1] = end_point
+		color_rect.position = end_point * 0.5
+		weight_label.text = str(weight)
+		default_color = line_color
+
+# returns vertices and weight
+func get_route_info():
+	var origin = get_parent()
+	pass
+	
