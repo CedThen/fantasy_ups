b5f1f66d009bd0dcd2141abb7c6306057394a8b1
JoshYaw <josh.yaw@glu.com>
2025-05-15 22:11:28 -0400

    Prelim test styles for dialogic QuestBox and NpcChat

--- scripts/main_hud.gd
+++ scripts/main_hud.gd
@@ -5,8 +5,8 @@ class_name MainHud extends CanvasLayer
 @onready var inventory_button: TextureButton = %InventoryButton
 @onready var quest_log_button: TextureButton = %QuestLogButton
 @onready var journal_button: TextureButton = %JournalButton
-#@onready var location_map_button: TextureButton = %LocationMapButton
-#@onready var calendar_button: TextureButton = %CalendarButton
+@onready var location_map_button: TextureButton = %LocationMapButton
+@onready var calendar_button: TextureButton = %CalendarButton
 
 var show_quest_box:bool = true
 var show_location_map:bool = true
@@ -34,8 +34,8 @@ func is_everything_out() -> bool:
 		world_map_button.is_out() and \
 		quest_log_button.is_out() and \
 		inventory_button.is_out() and \
-		#location_map_button.is_out() and \
-		#calendar_button.is_out() and \
+		location_map_button.is_out() and \
+		calendar_button.is_out() and \
 		journal_button.is_out():
 		return true
 		
@@ -46,8 +46,8 @@ func is_everything_in() -> bool:
 	if world_map_button.is_in() and \
 		inventory_button.is_in() and \
 		quest_log_button.is_in() and \
-		#calendar_button.is_in() and \
-		#location_map_button.is_in() and \
+		calendar_button.is_in() and \
+		location_map_button.is_in() and \
 		journal_button.is_in():
 		if show_quest_box:
 			if quest_box_button.is_in():
@@ -73,8 +73,8 @@ func open(quest_box:bool = true, location_map:bool = true) -> void:
 	inventory_button.intro()
 	quest_log_button.intro()
 	journal_button.intro()
-	#location_map_button.intro()
-	#calendar_button.intro()
+	location_map_button.intro()
+	calendar_button.intro()
 
 
 func close() -> void:
@@ -84,8 +84,8 @@ func close() -> void:
 	inventory_button.outro()
 	quest_log_button.outro()
 	journal_button.outro()
-	#location_map_button.outro()
-	#calendar_button.outro()
+	location_map_button.outro()
+	calendar_button.outro()
 
 
 func on_world_map_pressed():
