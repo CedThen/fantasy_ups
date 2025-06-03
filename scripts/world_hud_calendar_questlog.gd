05f4c8d68cb01ba09b70f05475feb22945281be6
JoshYaw <josh.yaw@glu.com>
2025-06-03 18:49:06 -0400

    added calendar and questlog stuff correctly

--- scripts/world_hud_calendar_questlog.gd
+++ scripts/world_hud_calendar_questlog.gd
@@ -6,8 +6,10 @@ var last_tab: WorldHudTab
 @onready var calendar_tab: WorldHudTab = %CalendarTab
 @onready var questlog_tab: WorldHudTab = %QuestlogTab
 
-@onready var animation_player: HudAnimationPlayer = %AnimationPlayer
-@onready var label: Label = %Label
+@onready var calendar_animation_player: HudAnimationPlayer = %CalendarAnimationPlayer
+@onready var calendar_label: Label = %CalendarLabel
+@onready var questlog_animation_player: HudAnimationPlayer = %QuestlogAnimationPlayer
+@onready var questlog_label: Label = %QuestlogLabel
 
 func _ready() -> void:
 	calendar_tab.tab_pressed.connect(on_tab_pressed)
@@ -19,25 +21,26 @@ func _ready() -> void:
 func close_other_tabs(current_tab:WorldHudTab = null):
 	if current_tab != calendar_tab:
 		calendar_tab.animation_player.outro()
+		calendar_animation_player.outro()
 	if current_tab != questlog_tab:
 		questlog_tab.animation_player.outro()
+		questlog_animation_player.outro()
 
 
 func populate_main_content(tab:WorldHudTab):
 	if tab == calendar_tab:
-		label.text = "CALENDAR"
+		calendar_animation_player.intro()
+		calendar_label.text = "CALENDAR"
 	elif tab == questlog_tab:
-		label.text = "QUESTS"
+		questlog_animation_player.intro()
+		calendar_label.text = "QUESTS"
 
 
 func on_tab_pressed(tab:WorldHudTab, opening:bool):
 	if opening:
 		close_other_tabs(tab)
-		if last_tab == null:
-			animation_player.intro()
 		last_tab = tab
 		populate_main_content(tab)
 	else:
 		close_other_tabs()
-		animation_player.outro()
 		last_tab = null
--- scripts/world_hud_inventory.gd
+++ scripts/world_hud_inventory.gd
@@ -6,6 +6,7 @@ extends Control
 
 @onready var inventory_button: TextureButton = %InventoryButton
 @onready var inventory_button_animation_player: HudAnimationPlayer = %InventoryButtonAnimationPlayer
+@onready var inventory_type_label: Label = %InventoryTypeLabel
 
 @export var inventory_button_active_img:Texture2D
 @export var inventory_button_inactive_img:Texture2D
@@ -19,6 +20,9 @@ func _ready() -> void:
 	
 	inventory_button_animation_player.animation_finished.connect(on_inventory_animation_finished)
 	last_tab = backpack_tab
+	
+
+	
 
 
 func open() -> void:
@@ -27,6 +31,7 @@ func open() -> void:
 
 func close() -> void:
 	pass
+
 
 # Tabs
 func close_other_tabs(current_tab:WorldHudTab = null):
@@ -38,10 +43,22 @@ func close_other_tabs(current_tab:WorldHudTab = null):
 		courier_tab.animation_player.outro()
 
 
+func populate_inventory(tab:WorldHudTab):
+	var player_inventory:InventoryData = Global.player.inventory
+	
+	if tab == backpack_tab:
+		inventory_type_label.text = "Backpack"
+	elif tab == pouch_tab:
+		inventory_type_label.text = "Pouch"
+	elif tab == courier_tab:
+		inventory_type_label.text = "Courier"
+
+
 func on_tab_pressed(tab:WorldHudTab, opening:bool):
 	if opening:
 		close_other_tabs(tab)
 		last_tab = tab
+		populate_inventory(tab)
 	else:
 		close_other_tabs()
 		inventory_button_animation_player.outro()
@@ -53,6 +70,7 @@ func on_inventory_animation_finished(anim_name:String):
 	if anim_name == Global.ANIM_NAME_INTRO:
 		inventory_button.texture_normal = inventory_button_active_img
 		last_tab.animation_player.intro()
+		populate_inventory(last_tab)
 	elif anim_name == Global.ANIM_NAME_OUTRO:
 		inventory_button.texture_normal = inventory_button_inactive_img
 
@@ -60,9 +78,6 @@ func on_inventory_animation_finished(anim_name:String):
 func _on_inventory_button_pressed() -> void:
 	if inventory_button_animation_player.anim_state == Global.AnimState.OUT:
 		inventory_button_animation_player.intro()
-		
 	elif inventory_button_animation_player.anim_state == Global.AnimState.IN:
 		inventory_button_animation_player.outro()
 		close_other_tabs()
-		
-	pass # Replace with function body.
